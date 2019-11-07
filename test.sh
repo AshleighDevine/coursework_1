364d36f133dcdf4d2dc5cb2128181331c79600e5
ashleighdevine <ashleighdevine95@hotmail.com>
2019-11-07 20:35:19 +0000

    Test Formatting Updated

--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
-echo "test 1"
+echo "Test 1"
 java Dec2Hex -1
-echo "test 2"
+
+echo "Test 2"
 java Dec2Hex A
-echo "test 3"
+
+echo "Test 3"
 java Dec2Hex @
-echo "test 4"
+
+echo "Test 4"
 java Dec2Hex 299
-echo "end of testing"
+
+echo "End of Testing"
